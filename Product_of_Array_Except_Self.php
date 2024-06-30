0370b17102d654f5c17a42292f0a1faf34058eda
baglanz <baglan.zhaksylyk98@gmail.com>
2024-06-30 18:09:12 +0500

    Product of Array Except Self

---
+++ Product_of_Array_Except_Self.php
@@ -0,0 +1,51 @@
+Product of Array Except Self
+
+Description:
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the
+product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+
+//Input: nums = [1,2,3,4]
+//Output: [24,12,8,6]
+    function productExceptSelf($nums) {
+        $n = count($nums);
+        $answer = array_fill(0, $n, 1);
+
+        $prefix = 1;
+        for ($i = 0; $i < $n; $i++) {
+            $answer[$i] = $prefix;
+            $prefix *= $nums[$i];
+        }
+
+        $suffix = 1;
+        for ($i = $n - 1; $i >= 0; $i--) {
+            $answer[$i] *= $suffix;
+            $suffix *= $nums[$i];
+        }
+
+        return $answer;
+    }
+}
+?>
+
+See on leetcode.com: https://leetcode.com/problems/product-of-array-except-self/description
